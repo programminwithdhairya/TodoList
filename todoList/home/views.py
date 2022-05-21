403257d0b36ba3594c9a2291aa1855e5d8e6a3d1
programminwithdhairya <105724103+programminwithdhairya@users.noreply.github.com>
2022-05-21 14:50:26 +0530

    Add files via upload

---
+++ todoList/home/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render  , HttpResponse
+from home.models import Task
+
+# Create your views here.
+def home(request):
+    context = {"success" : False}
+    if request.method == 'POST':
+        # Handle the form
+        title = request.POST['title']    
+        desc = request.POST['desc']    
+        ins = Task(taskTitle = title , taskDesc = desc)
+        ins.save()
+        context = {"success" : True}
+
+    return render(request , 'index.html' , context)
+
+def tasks(request):
+    allTasks = Task.objects.all()
+    # print(allTasks)
+    # for item in allTasks:
+    #     print(item.taskDesc)
+    context = {'tasks' : allTasks}
+    return render(request , 'tasks.html' , context)
+    # form validation  , update , delete 
